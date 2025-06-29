405e8b08a1442020938254e07396f634a0b77c87
Denis Nagovitsyn <disochkin@gmail.com>
2025-06-30 01:48:58 +0500

    code style fix

--- src/test/java/ru/yandex/practicum/filmorate/UserModelTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserModelTest.java
@@ -9,10 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserModelTest {
     User user;
+
     @Test
     void userModelCommonTest() {
         User user = new User(1L, "usermail@mail.ru", "user-login", "user-name",
-                LocalDate.of(1980, 1, 10) );
+                LocalDate.of(1980, 1, 10));
         assertEquals(1L, user.getId());
         assertEquals("usermail@mail.ru", user.getEmail());
         assertEquals("user-login", user.getLogin());
