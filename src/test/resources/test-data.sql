86fb84dca720ab8be591d8571ee489a7f87e07b0
Denis Nagovitsyn <disochkin@gmail.com>
2025-08-12 15:03:35 +0500

    Обновил test-data.sql

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,17 +1,14 @@
 -- Тестовые пользователи
 INSERT INTO USERS (EMAIL,LOGIN,NAME,BIRTHDAY) VALUES
-('email_1@domain.ru','testUserLogin1','testUserName1','2001-01-01');
-INSERT INTO USERS (EMAIL,LOGIN,NAME,BIRTHDAY) VALUES
-('email_2@domain.ru','testUserLogin2','testUserName2','2002-02-02');
-INSERT INTO USERS (EMAIL,LOGIN,NAME,BIRTHDAY) VALUES
+('email_1@domain.ru','testUserLogin1','testUserName1','2001-01-01'),
+('email_2@domain.ru','testUserLogin2','testUserName2','2002-02-02'),
 ('email_3@domain.ru','testUserLogin3','testUserName3','2003-03-03');
 
 -- Тестовые фильмы
 INSERT INTO MPA (ID, NAME) VALUES
 (1,'G');
+
 INSERT INTO FILM (NAME,DESCRIPTION,RELEASE_DATE,DURATION,MPA_ID) VALUES
-('film1', 'film description1', '2001-01-01', '10', 1);
-INSERT INTO FILM (NAME,DESCRIPTION,RELEASE_DATE,DURATION,MPA_ID) VALUES
-('film2', 'film description2', '2002-02-02', '20', 1);
-INSERT INTO FILM (NAME,DESCRIPTION,RELEASE_DATE,DURATION,MPA_ID) VALUES
+('film1', 'film description1', '2001-01-01', '10', 1),
+('film2', 'film description2', '2002-02-02', '20', 1),
 ('film3', 'film description3', '2003-03-03', '30', 1);
